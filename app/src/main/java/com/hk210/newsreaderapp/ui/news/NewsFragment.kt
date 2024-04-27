4f1a86a6d8dcb2c3bf5110576b8e3348989492a3
Harsh <hk21051999@gmail.com>
2024-04-28 02:55:12 +0530

    feature/fcm - Removed unused imports

--- app/src/main/java/com/hk210/newsreaderapp/ui/news/NewsFragment.kt
+++ app/src/main/java/com/hk210/newsreaderapp/ui/news/NewsFragment.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.widget.SearchView.OnQueryTextListener
 import androidx.browser.customtabs.CustomTabsIntent
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
